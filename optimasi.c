2a69413635c3880fb1005af9d0a78eb778e490ca
abdan <abdansya34@gmail.com>
2026-06-08 10:07:48 +0700

    Enhance energy optimization calculations and reporting

--- optimasi.c
+++ optimasi.c
@@ -50,25 +50,79 @@ void optimizeEnergy(Device arr[],
                     int n,
                     int maxPower)
 {
-    int total = 0;
+    int totalPower = 0;
+    float totalCost = 0;
 
-    printf("\n=== REKOMENDASI PERANGKAT ===\n");
+    const float TARIF = 1444.70;
 
-    for(int i=0;i<n;i++)
+    printf("\n====================================\n");
+    printf("      LAPORAN AUDIT ENERGI\n");
+    printf("====================================\n");
+
+    for(int i = 0; i < n; i++)
     {
-        if(total + arr[i].power <= maxPower)
+        arr[i].dailyKwh =
+            (arr[i].power * arr[i].hoursPerDay) / 1000.0;
+
+        arr[i].monthlyCost =
+            arr[i].dailyKwh * 30 * TARIF;
+
+        totalCost += arr[i].monthlyCost;
+
+        printf("\nPerangkat : %s\n", arr[i].name);
+        printf("Daya      : %d Watt\n",
+               arr[i].power);
+        printf("Pemakaian : %d Jam/Hari\n",
+               arr[i].hoursPerDay);
+        printf("Konsumsi  : %.2f kWh/Hari\n",
+               arr[i].dailyKwh);
+        printf("Biaya     : Rp %.0f/Bulan\n",
+               arr[i].monthlyCost);
+    }
+
+    printf("\n====================================\n");
+    printf("TOTAL ESTIMASI TAGIHAN : Rp %.0f\n",
+           totalCost);
+
+    printf("\n====================================\n");
+    printf("REKOMENDASI PENGGUNAAN DAYA\n");
+    printf("====================================\n");
+
+    for(int i = 0; i < n; i++)
+    {
+        if(totalPower + arr[i].power <= maxPower)
         {
             printf("%s (%d Watt)\n",
                    arr[i].name,
                    arr[i].power);
 
-            total += arr[i].power;
+            totalPower += arr[i].power;
         }
     }
 
     printf("\nTotal Daya Digunakan : %d Watt\n",
-           total);
+           totalPower);
 
     printf("Batas Daya Rumah     : %d Watt\n",
            maxPower);
+
+    printf("\n====================================\n");
+    printf("SARAN PENGHEMATAN\n");
+    printf("====================================\n");
+
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i].hoursPerDay > 4)
+        {
+            float hemat =
+            ((arr[i].power * 2) / 1000.0)
+            * 30 * TARIF;
+
+            printf("- Kurangi penggunaan %s 2 jam/hari\n",
+                   arr[i].name);
+
+            printf("  Potensi hemat Rp %.0f/bulan\n",
+                   hemat);
+        }
+    }
 }
